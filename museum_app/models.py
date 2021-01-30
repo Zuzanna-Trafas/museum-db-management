fa6574972ac8f613ca8c9d414276132ecaf3c808
Zuzanna-Trafas <zuz.trafas@gmail.com>
2021-01-30 11:57:21 +0100

    working version with bootstrap added

--- museum_app/models.py
+++ museum_app/models.py
@@ -1,32 +1,40 @@
 from django.db import models
-#TODO: on_delete
-#TODO: missing verboses
-#TODO: unique? unique_for_data? Who knows, not me
-#TODO: length validators
-#TODO: primary keys fix
+
+
+# TODO: on_delete
+# TODO: missing verboses
+# TODO: unique? unique_for_data? Who knows, not me
+# TODO: length validators
+# TODO: primary keys fix
 
 class Oddzial(models.Model):
-        nazwa = models.CharField(max_length=100, primary_key=True)
-        godzina_otwarcia = models.TimeField()
-        godzina_zamkniecia = models.TimeField(verbose_name="godzina zamknięcia")
-        adres = models.CharField(max_length=100)
-        numer_telefonu = models.PositiveIntegerField()
+    nazwa = models.CharField(max_length=100, primary_key=True)
+    godzina_otwarcia = models.TimeField()
+    godzina_zamkniecia = models.TimeField(verbose_name="godzina zamknięcia")
+    adres = models.CharField(max_length=100)
+    numer_telefonu = models.PositiveIntegerField()
+
 
 class Wydarzenie(models.Model):
     nazwa = models.CharField(max_length=100, primary_key=True)
     data_rozpoczecia = models.DateField(verbose_name="data rozpoczęcia")
     data_zakonczenia = models.DateField(verbose_name="data zakończenia")
 
+
 class Wydarzenie_oddzial(models.Model):
-    oddzial_nazwa = models.OneToOneField(Oddzial, on_delete=models.CASCADE, primary_key=True) 
-    wydarzenie_nazwa = models.OneToOneField(Wydarzenie, on_delete=models.CASCADE, related_name='wydarzenie_oddzial_wydarzenie_nazwa')
-    wydarzenie_data_rozpoczecia = models.OneToOneField(Wydarzenie, on_delete=models.CASCADE, related_name='wydarzenie_oddzial_wydarzenie_data_rozpoczecia')
+    oddzial_nazwa = models.OneToOneField(Oddzial, on_delete=models.CASCADE, primary_key=True)
+    wydarzenie_nazwa = models.OneToOneField(Wydarzenie, on_delete=models.CASCADE,
+                                            related_name='wydarzenie_oddzial_wydarzenie_nazwa')
+    wydarzenie_data_rozpoczecia = models.OneToOneField(Wydarzenie, on_delete=models.CASCADE,
+                                                       related_name='wydarzenie_oddzial_wydarzenie_data_rozpoczecia')
+
 
 class Rodzaj_biletu(models.Model):
     typ = models.CharField(max_length=100, primary_key=True)
     czy_z_przewodnikiem = models.BooleanField()
     cena = models.FloatField()
     oddzial_nazwa = models.OneToOneField(Oddzial, on_delete=models.CASCADE)
+
 
 class Pracownik(models.Model):
     pesel = models.PositiveIntegerField(primary_key=True)
@@ -38,19 +46,26 @@ class Pracownik(models.Model):
     numer_telefonu = models.PositiveIntegerField()
     oddzial_nazwa = models.ForeignKey(Oddzial, on_delete=models.CASCADE)
 
+
 class Harmonogram_zwiedzania(models.Model):
     id = models.PositiveIntegerField(primary_key=True)
     godzina_rozpoczecia = models.TimeField(unique=True)
     data = models.DateField(unique=True)
     pracownik_pesel = models.OneToOneField(Pracownik, on_delete=models.CASCADE)
 
+
 class Bilet(models.Model):
     id = models.PositiveIntegerField(primary_key=True)
     data_zakupu = models.DateField()
-    rodzaj_biletu_typ = models.ForeignKey(Rodzaj_biletu, related_name='bilet_rodzaj_biletu_typ', on_delete=models.CASCADE)
-    rodzaj_biletu_oddzial_nazwa = models.ForeignKey(Rodzaj_biletu, related_name='bilet_rodzaj_biletu_oddzial_nazwa', on_delete=models.CASCADE)
-    rodzaj_biletu_czy_z_przewodnikiem = models.ForeignKey(Rodzaj_biletu, related_name='bilet_rodzaj_biletu_czy_z_przewodnikiem', on_delete=models.CASCADE)
+    rodzaj_biletu_typ = models.ForeignKey(Rodzaj_biletu, related_name='bilet_rodzaj_biletu_typ',
+                                          on_delete=models.CASCADE)
+    rodzaj_biletu_oddzial_nazwa = models.ForeignKey(Rodzaj_biletu, related_name='bilet_rodzaj_biletu_oddzial_nazwa',
+                                                    on_delete=models.CASCADE)
+    rodzaj_biletu_czy_z_przewodnikiem = models.ForeignKey(Rodzaj_biletu,
+                                                          related_name='bilet_rodzaj_biletu_czy_z_przewodnikiem',
+                                                          on_delete=models.CASCADE)
     harmonogram_zwiedzania_id = models.ForeignKey(Harmonogram_zwiedzania, on_delete=models.CASCADE)
+
 
 class Dzial(models.Model):
     nazwa = models.CharField(max_length=100, primary_key=True)
@@ -58,12 +73,14 @@ class Dzial(models.Model):
     epoka = models.CharField(max_length=100)
     oddzial_nazwa = models.OneToOneField(Oddzial, on_delete=models.CASCADE)
 
+
 class Artysta(models.Model):
     id = models.PositiveIntegerField(primary_key=True)
     imie = models.CharField(max_length=50, verbose_name="imię")
     nazwisko = models.CharField(max_length=50)
     data_urodzenia = models.DateField()
     data_śmierci = models.DateField(verbose_name="data śmierci")
+
 
 class Obraz(models.Model):
     id = models.PositiveIntegerField(primary_key=True)
@@ -73,7 +90,8 @@ class Obraz(models.Model):
     artysta_id = models.ForeignKey(Artysta, on_delete=models.CASCADE)
     dzial_nazwa = models.ForeignKey(Dzial, on_delete=models.CASCADE, related_name='obraz_dzial_nazwa')
     dzial_oddzial_nazwa = models.ForeignKey(Dzial, on_delete=models.CASCADE, related_name='obraz_dzial_oddzial_nazwa')
-    
+
+
 class Rzezba(models.Model):
     id = models.PositiveIntegerField(primary_key=True)
     nazwa = models.CharField(max_length=100)
@@ -82,7 +100,3 @@ class Rzezba(models.Model):
     artysta_id = models.ForeignKey(Artysta, on_delete=models.CASCADE)
     dzial_nazwa = models.ForeignKey(Dzial, on_delete=models.CASCADE, related_name='rzezba_dzial_nazwa')
     dzial_oddzial_nazwa = models.ForeignKey(Dzial, on_delete=models.CASCADE, related_name='rzezba_dzial_oddzial_nazwa')
-    
-
-
-
--- museum_app/views.py
+++ museum_app/views.py
@@ -1,35 +1,46 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 def main(request):
-        return render(request, 'museum_app/main.html')
+    return render(request, 'museum_app/main.html')
+
 
 def oddzialy(request):
-        return render(request, 'museum_app/oddzialy.html')
+    return render(request, 'museum_app/oddzialy.html')
+
 
 def dzialy(request):
-        return render(request, 'museum_app/dzialy.html')
+    return render(request, 'museum_app/dzialy.html')
+
 
 def dziela(request):
-        return render(request, 'museum_app/dziela.html')
+    return render(request, 'museum_app/dziela.html')
+
 
 def artysci(request):
-        return render(request, 'museum_app/artysci.html')
+    return render(request, 'museum_app/artysci.html')
+
 
 def rodzaje_biletow(request):
-        return render(request, 'museum_app/rodzaje_biletow.html')
+    return render(request, 'museum_app/rodzaje_biletow.html')
+
 
 def bilety(request):
-        return render(request, 'museum_app/bilety.html')
+    return render(request, 'museum_app/bilety.html')
+
 
 def pracownicy(request):
-        return render(request, 'museum_app/pracownicy.html')
+    return render(request, 'museum_app/pracownicy.html')
+
 
 def harmonogram_zwiedzania(request):
-        return render(request, 'museum_app/harmonogram_zwiedzania.html')
+    return render(request, 'museum_app/harmonogram_zwiedzania.html')
+
 
 def add(request):
-        return render(request, 'museum_app/add.html')
+    return render(request, 'museum_app/add.html')
+
 
 def edit(request):
-        return render(request, 'museum_app/edit.html')
+    return render(request, 'museum_app/edit.html')
